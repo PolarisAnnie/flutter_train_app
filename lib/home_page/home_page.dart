5c720f6dd2c72a360b3bb48495a170fb52e28e4f
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-08 17:22:36 +0900

    feat: 홈페이지 뼈대 레이아웃, 위젯 구조별 폴더 정리

---
+++ lib/home_page/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_train_app/home_page/search_button.dart';
+import 'package:flutter_train_app/home_page/select_place.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //body background 색상
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(title: Text("기차 예매")),
+      // Scaffold body padding 20
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          //body 내 위젯 세로 가운데 정렬
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [SelectPlace(), SizedBox(height: 20), SearchButton()],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page/select_place.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class SelectPlace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('SelectPlace Widget');
+  }
+}
