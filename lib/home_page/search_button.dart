67b882593a819d6a86b98913092384b9850f3b97
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-08 17:46:00 +0900

    feat: 좌석 선택 버튼 UI 구현

--- lib/home_page/search_button.dart
+++ lib/home_page/search_button.dart
@@ -1,8 +1,30 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class SearchButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('SearchButton Widget');
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        // 버튼 스타일 설정(색상, 모서리 둥글기)
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.purple,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(20),
+          ),
+        ),
+        onPressed: () {},
+        // 텍스트 및 스타일 설정(색상, 크기, 두께)
+        child: Text(
+          "좌석 선택",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
