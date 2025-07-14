090c4b5d72638f7384fc15fe895714e18e5e18e0
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-14 16:08:25 +0900

    feat: 터치된 좌석이 있을 시에만 예매하기 버튼 활성화

--- lib/seat_page/seat_reservation_button.dart
+++ lib/seat_page/seat_reservation_button.dart
@@ -9,41 +9,48 @@ class SeatReservationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isSeatSelected = selectedRow != null && selectedCol != null;
     return Padding(
       padding: const EdgeInsets.only(top: 0, bottom: 30, left: 20, right: 20),
       child: SizedBox(
         width: double.infinity,
         height: 50,
-        //버튼 Theme 적용
         child: ElevatedButton(
-          onPressed: () {
-            showCupertinoDialog(
-              context: context,
-              builder: (context) => CupertinoAlertDialog(
-                title: Text("예매 하시겠습니까?"),
-                content: Text("좌석 : $selectedRow-$selectedCol"),
-                actions: [
-                  CupertinoDialogAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      Navigator.pop(context);
-                    },
-                    child: Text("확인", style: TextStyle(color: Colors.blue)),
-                  ),
-                  CupertinoDialogAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("취소", style: TextStyle(color: Colors.red)),
-                  ),
-                ],
-              ),
-            );
-          },
+          onPressed: isSeatSelected
+              ? () {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                      title: Text("예매 하시겠습니까?"),
+                      content: Text("좌석 : $selectedRow-$selectedCol"),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "확인",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "취소",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              : null, // 좌석이 선택되지 않으면 버튼 비활성화
           child: Text("예매 하기"),
         ),
       ),
     );
-    // 버튼 Theme 적용
   }
 }
